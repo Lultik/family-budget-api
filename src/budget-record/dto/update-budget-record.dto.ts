bdc46c7b41a076a62cfedb5cb961e7db51e05d1a
Mikhail_Stankevich <Mikhail_Stankevich@epam.com>
2025-05-26 00:24:10 +0200

    chore: remove budget-record

--- src/budget-record/dto/update-budget-record.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateBudgetRecordDto } from "./create-budget-record.dto";
-
-export class UpdateBudgetRecordDto extends PartialType(CreateBudgetRecordDto) {}
